9d5bc6dbfe378f3bf20ba6daa588cdead22f9409
Phan Thị Quỳnh Như <82049190+NHU0606@users.noreply.github.com>
2023-01-26 12:57:43 +0700

    haiz

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $("#header").load(`pages/layout/header.html`);
 
-  // $("#content").load(`pages/index.html`);
+  $("#content").load(`pages/index.html`);
 
-  $("#menu").load(`pages/layout/menu.html`);
+  // $("#menu").load(`pages/layout/menu.html`);
 
   $("#footer").load(`pages/layout/footer.html`);
 })
