80f5b7f3c7db60af258c0589a448eba71617eae8
Phan Thị Quỳnh Như <82049190+NHU0606@users.noreply.github.com>
2023-02-01 22:26:00 +0700

    make staging branch to write menu

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $("#header").load(`pages/layout/header.html`);
+
   $("#menu").load(`pages/layout/menu.html`)
   
-  $("#content").load(`pages/index.html`);
-  
-
-  // $("#menu").load(`pages/layout/menu.html`);
+  $("#content").load(`pages/newViral.html`);  
 
   $("#footer").load(`pages/layout/footer.html`);
-})
+});
--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,30 @@
+// we gonna change the content about list item for kind of product: slide of page indexedDB.html, teddy, doll, nothing 
+
+
 $(document).ready(function() {
-  // this line 4 slider appear
+  // this line for slider appear
   $("#sub-content").load(`pages/layout/slider.html`);
 
-  // this line 4 list doll appear
+  // for teddy
+//   $("#sub-content").load(`pages/teddy.html`);
+
+  // for list doll appear
   // $("#sub-content").load(`pages/doll.html`);
+
+  //for no products
+  // $("#sub-content").load(`pages/oop_noProduct.html`);
+
+
+  const items = document.querySelectorAll('.category-item')
+  const itemActive = document.querySelector('.category-item.category-item--active')
+
+    items.forEach(item => {
+        item.onclick = function() {
+          item.classList.add('category-item--active') 
+          itemActive.classList.remove('category-item--active')        
+        }
+    });
+
+    
+
 })
