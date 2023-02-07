2b901088c0420aaca1dafc5fb04d1edfd1dd59d3
LinhNguyen <linhnq236@gmail.com>
2023-02-07 23:08:48 +0700

    [development] suggest login page to dashboard

---
+++ js/admin/login.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // email, password value
+
+  // load users.json 
+
+  // if email && password >< users.json load
+
+  // if true (login in)
+
+  // save user instanceof Users.json localstorage set currentUser: user
+
+
+  // else false -> redirect to  login page 
+
+
+})
