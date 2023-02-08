0d4084daccfa99275d5086eddb21faf611100e87
Phan Thị Quỳnh Như <82049190+NHU0606@users.noreply.github.com>
2023-02-09 00:23:09 +0700

    done page see cart

--- js/see_cart.js
+++ js/see_cart.js
@@ -7,7 +7,10 @@ $(document).ready(function() {
         carts.forEach(itemCart => {
             seeCartItem += `
             <li class="cart_item">
-                <div class="col l-6 m-6 c-6 cart-item-left">
+                <div class="col l-4 m-6 c-12 cart-item-left" style=" display: flex;">
+                    <label for="checkbox1" class="check-product-form">
+                        <input id="check-product" type="checkbox" role="checkbox"/>
+                    </label>
                     <div class="cart_img-pt">
                         <img class="cart_img" src="${itemCart.image}" alt="" class="cart_img">
                     </div>
@@ -15,12 +18,20 @@ $(document).ready(function() {
                         <h2 class="cart_name">${itemCart.name}</h2>
                     </div>
                 </div>
-                <div class="col l-6 m-6 c-6 cart-item-rigth">
+                <div class="col l-8 m-6 c-12 cart-item-rigth" style=" display: flex;">
                     <div class="col l-2 cart_typeOf-pt">
-                        <span class="cart_typeOf">Type of: ${itemCart.category.text}</span>
+                        <span class="cart_typeOf">${itemCart.category.text}</span>
                     </div>
                     <div class="col l-2 cart_cost-pt">
                         <span class="cart_costNew">${itemCart.new_cost}</span>
+                    </div>
+                    <div class="col l-2 cart_quanlity-number">
+                        <i class="fa-solid fa-minus"></i>
+                        <span class="product-quanlity">1</span>
+                        <i class="fa-solid fa-plus"></i>
+                    </div>
+                    <div class="col l-2 cart_totalCost">
+                        <span class="cart-totalCost">Total Cost</span>
                     </div>
                     <div class="col l-2 cart_del-pt">
                         <span class="cart_delete">Delete</span>                                                         
