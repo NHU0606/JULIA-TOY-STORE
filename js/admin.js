187d9a5cb631a5968674da0690ed20200fdfe0af
LinhNguyen <linhnq236@gmail.com>
2023-02-09 23:31:56 +0700

    login admin and set layout admin

---
+++ js/admin.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  let currentUser = JSON.parse(localStorage.getItem('currentUser')) || undefined;
+  if (currentUser) {
+    $("#body").load(`pages/admin/dashboard.html`);  
+  } else {
+    $("#body").load(`pages/admin/login.html`);  
+  }
+});
--- js/login.js
+++ js/login.js
@@ -2,19 +2,23 @@ $(document).ready(function() {
     const mailInput = document.getElementById('email-input')
     const passInput = document.getElementById('pass-input')
     const loginBtn = document.getElementById('login-btn')
-    
+
+    let bodyEl = document.getElementById("body");
+    bodyEl.style.height = (window.innerHeight - 120) + 'px';
+
+    window.addEventListener("resize", function() {
+        bodyEl.style.height = (window.innerHeight - 120) + 'px';
+    });
+
 
     fetch("../json/users.json")
     .then(response => response.json())
     .then(loginArray => {
-        let user = JSON.parse(localStorage.getItem('user')) || []
-
         loginArray.forEach(item => {
             loginBtn.addEventListener('click', e => {
                 if (mailInput.value === item.email && passInput.value === item.password) {
-                    user.push(loginArray)
-                    localStorage.setItem('user', JSON.stringify(user))
-                    window.location.href = "pages/admin/dashboard.html#"
+                    localStorage.setItem('currentUser', JSON.stringify(storeUser(item)))
+                    window.location.reload();
                 } else if (mailInput.value === '') {
                     alert('Press Email to login')
                 } else if (passInput.value === '') {
@@ -22,11 +26,14 @@ $(document).ready(function() {
                 }
             })
         });
-
-        if(user) {
-            window.location.href = "pages/admin/dashboard.html#"
-        } else {
-            window.location.href = "admin.html"
-        }
     })
+
+    function storeUser(user) {
+        let _user = {};
+        _user['email'] = user.email;
+        _user['id'] = user.id;
+        _user['role'] = user.role;
+        _user['name'] = user.name;
+        return _user;
+    }
 })
